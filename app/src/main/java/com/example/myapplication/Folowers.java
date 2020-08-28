29256d2d1ca8172a05f0c6796381fd5a1f24b592
widatcho509 <59575077+widatcho509@users.noreply.github.com>
2020-08-28 18:36:28 -0300

    this is the new activity for followers section 2

--- app/src/main/java/com/example/myapplication/Folowers.java
+++ app/src/main/java/com/example/myapplication/Folowers.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Folowers extends AppCompatActivity {
-
+//This is the followers part 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
